6e3048539b663adb2d5e28359cdb17eb294fec32
Daria <dashaalex@>
2025-09-02 22:00:28 +0300

    edit for linux

--- ConsoleApplication28.cpp
+++ ConsoleApplication28.cpp
@@ -4,22 +4,22 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <windows.h>
+//#include <boost/filesystem.hpp>
 
-void read_directory(const std::string& name, std::vector<std::string>& v)
-{
-    std::string pattern(name);
-    pattern.append("\\*");
-    WIN32_FIND_DATA data;
-    HANDLE hFind;
-    if ((hFind = FindFirstFile(pattern.c_str(), &data)) != INVALID_HANDLE_VALUE) {
-        do {
-            v.push_back(data.cFileName);
-        } while (FindNextFile(hFind, &data) != 0);
-        FindClose(hFind);
-    }
-}
+namespace fs = std::filesystem;
+// namespace fsb = boost::filesystem;
 
+// struct path_leaf_string {
+//     std::string operator () (const fsb::directory_entry& entry)const {
+//             return entry.path().leaf().string();
+//     }
+// };
+
+// void read_directory(const std::string& path, std::vector<std::string>& v){
+//     fsb::path p(path);
+//     fsb::directory_iterator start(p);
+//     fs::directory_iterator end;
+// }
 struct Directory {
     std::string path_;
     bool is_directory;
@@ -30,13 +30,15 @@ struct Directory {
 int main()
 {
     std::vector<std::string> v;
-    read_directory(".", v);
-    std::copy(v.begin(), v.end(),
-        std::ostream_iterator<std::string>(std::cout, "\n"));
-    std::string path1;
-    std::string path2;
-    std::cout << "Enter path to first directory: ";
-    std::cin >> path1;
-    std::cout << "Enter path to second directory: ";
-    std::cin >> path2;
+    //read_directory(".",v);
+
+
+    // std::string path1;
+    // std::string path2;
+    // std::cout << "Enter path to first directory: ";
+    // std::cin >> path1;
+    // std::cout << "Enter path to second directory: ";
+    // std::cin >> path2;
+    // fs::create_directory(path1);
+    // fs::create_directory(path1);
 }
